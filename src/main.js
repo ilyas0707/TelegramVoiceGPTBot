e9d373aa11bd13f54948a76946183950a8628db1
Ilias Iangurazov <ilias_iangurazov@epam.com>
2024-01-06 17:36:12 +0600

    init

---
+++ src/main.js
@@ -0,0 +1,76 @@
+import { Telegraf, session } from 'telegraf'
+import { message } from 'telegraf/filters'
+import config from 'config'
+import mongoose from 'mongoose'
+import {
+    proccessVoiceMessage,
+    proccessTextMessage,
+    handleCallbackQuery,
+    getUserConversations,
+} from './logic.js'
+import { initCommand, normalize } from './utils.js'
+
+console.log(config.get('TEST_ENV'))
+
+const bot = new Telegraf(config.get('TELEGRAM_TOKEN'), {
+    handlerTimeout: Infinity,
+})
+
+bot.use(session())
+bot.use(normalize())
+
+bot.command(
+    'new',
+    initCommand('Начат новый диалог. Жду голосовое или текстовое сообщение.')
+)
+
+bot.command(
+    'start',
+    initCommand(
+        'Добро пожаловать в бота. Отправьте голосовое или текстовое сообщение для общения с ChatGPT.'
+    )
+)
+
+bot.command('history', getUserConversations)
+
+bot.command('admin', async (ctx) => {
+    if (ctx.message.from.id !== config.get('ADMIN_TG_ID')) return
+    await ctx.reply('Привет Владилен')
+    // ctx.sendAudio()
+})
+
+bot.on(message('voice'), proccessVoiceMessage)
+
+bot.on(message('text'), proccessTextMessage)
+
+bot.on('callback_query', handleCallbackQuery)
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('MONGODB_URI'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+
+        bot.launch()
+
+        console.log('MongoDB Connected and bot started.')
+
+        process.on('uncaughtException', (err) => {
+            console.error('Неперехваченное исключение:', err)
+            // process.exit(1)
+        })
+
+        process.on('unhandledRejection', (reason, promise) => {
+            console.error({ unhandledRejection: { reason, promise } })
+        })
+
+        // process.once('SIGINT', () => bot.stop('SIGINT'))
+        // process.once('SIGTERM', () => bot.stop('SIGTERM'))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
---
+++ src/openai.js
@@ -0,0 +1,60 @@
+import { OpenAI } from 'openai'
+import { createReadStream } from 'fs'
+import config from 'config'
+
+const CHAT_GPT_MODEL = 'gpt-3.5-turbo'
+
+class OpenAi {
+    roles = {
+        ASSISTANT: 'assistant',
+        SYSTEM: 'system',
+        USER: 'user',
+    }
+
+    constructor(apiKey) {
+        this.openai = new OpenAI({
+            apiKey,
+        })
+    }
+
+    async chat(messages = [], user = '') {
+        try {
+            const completion = await this.openai.chat.completions.create({
+                model: CHAT_GPT_MODEL,
+                messages,
+                user,
+            })
+
+            console.log('Usage', completion.usage)
+
+            return completion.choices[0].message
+        } catch (e) {
+            console.error(`Error while chat completion: ${e.message}`)
+        }
+    }
+
+    async transcription(filepath) {
+        try {
+            const response = await this.openai.audio.transcriptions.create(
+                {
+                    file: createReadStream(filepath),
+                    model: 'whisper-1',
+                }
+            );
+            return response.text;
+        } catch (e) {
+            console.error(`Error while transcription: ${e.message}`);
+        }
+    }
+
+    // async image(prompt = 'a white siamese cat') {
+    //     const response = await this.openai.images.generate({
+    //         prompt,
+    //         n: 1,
+    //         size: '1024x1024',
+    //     })
+    //     return response.data.data[0].url
+    // }
+}
+
+export const openai = new OpenAi(config.get('OPENAI_KEY'))
