d2edd808adafc24ed48673fbd598b6f6e052df39
Ilias Iangurazov <ilias_iangurazov@epam.com>
2024-01-06 18:53:57 +0600

    init

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-import { startVercel } from '../src';
+import { startVercel } from '../src/index.js';
 
 export default async function handle(req, res) {
     try {
